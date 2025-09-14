f958bee3d16a1fa66d41eee2abad9976ca83e0a4
kietsocola <vankiet27012004@gmail.com>
2025-09-15 00:26:05 +0700

    feat: fix

--- lti-service-python/app/config.py
+++ lti-service-python/app/config.py
@@ -30,27 +30,33 @@ class Settings(BaseSettings):
     JWT_EXPIRATION: int = 3600
     
     # LTI 1.3
-    LTI_CLIENT_ID: str = "ilWFql3vODVfqMp"
+    LTI_CLIENT_ID: str = "8gSjDiDrD4x2ZpS"
     LTI_DEPLOYMENT_ID: str = "1"
-    LTI_ISSUER: str = "http://localhost/moodle"
-    LTI_AUTH_URL: str = "http://localhost/moodle/mod/lti/auth.php"
-    LTI_TOKEN_URL: str = "http://localhost/moodle/mod/lti/token.php"
-    LTI_KEYSET_URL: str = "http://localhost/moodle/mod/lti/certs.php"
+    LTI_ISSUER: str = "http://51.68.124.207/moodle"
+    LTI_AUTH_URL: str = "http://51.68.124.207/moodle/mod/lti/auth.php"
+    LTI_TOKEN_URL: str = "http://51.68.124.207/moodle/mod/lti/token.php"
+    LTI_KEYSET_URL: str = "http://51.68.124.207/moodle/mod/lti/certs.php"
     
     # Tool Configuration
     TOOL_TITLE: str = "User Log Viewer"
     TOOL_DESCRIPTION: str = "View user activity logs from Moodle"
-    TOOL_TARGET_LINK_URI: str = "http://localhost:8082/lti/launch"
-    TOOL_OIDC_INITIATION_URL: str = "http://localhost:8082/lti/login"
-    TOOL_PUBLIC_JWK_URL: str = "http://localhost:8082/lti/jwks"
+    TOOL_TARGET_LINK_URI: str = "http://51.68.124.207:8082/lti/launch"
+    TOOL_OIDC_INITIATION_URL: str = "http://51.68.124.207:8082/lti/login"
+    TOOL_PUBLIC_JWK_URL: str = "http://51.68.124.207:8082/lti/jwks"
     
     # Moodle API
-    MOODLE_API_URL: str = "http://localhost/moodle/webservice/rest/server.php"
+    MOODLE_API_URL: str = "http://51.68.124.207/moodle/webservice/rest/server.php"
     MOODLE_API_TOKEN: str = "9b13f135bae7ba27d67e609c414b70df"
     
     # Security
-    ALLOWED_HOSTS: List[str] = ["localhost", "127.0.0.1"]
-    CORS_ORIGINS: List[str] = ["http://localhost:5173", "http://localhost:8080"]
+    ALLOWED_HOSTS: List[str] = ["localhost", "127.0.0.1", "51.68.124.207"]
+    CORS_ORIGINS: List[str] = [
+        "http://localhost:5173",
+        "http://localhost:8080",
+        "http://51.68.124.207:5173",
+        "http://51.68.124.207:8080"
+    ]
+
     
     # Logging
     LOG_LEVEL: str = "INFO"
